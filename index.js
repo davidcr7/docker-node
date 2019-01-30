c2dfd15cdaa1e8c697d4815855b584f5aefab5c5
davidcr <1070631407@qq.com>
2019-01-30 16:51:05 +0800

    1

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(req,res){
    res.writeHead(200,{"Content-type":"text/blain"});
-   res.write("Hello My love NodeJs 啊啊啊as");
+   res.write("Hello My love NodeJs lalala");
    res.end();
 }).listen(8080);
