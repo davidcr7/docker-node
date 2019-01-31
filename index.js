4595b605e0e7fcf14dcf264f737868e65383b4e2
davidcr <1070631407@qq.com>
2019-01-31 11:38:21 +0800

    12313

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(req,res){
    res.writeHead(200,{"Content-type":"text/blain"});
-   res.write("Cristiano Ronaldo");
+   res.write("Cristiano Ronaldo DavidCR 7");
    res.end();
 }).listen(8080);
