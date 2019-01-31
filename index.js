3afcc74e156ef57d27d37c12181adf5404438ae0
davidcr <1070631407@qq.com>
2019-01-31 10:43:18 +0800

    1

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(req,res){
    res.writeHead(200,{"Content-type":"text/blain"});
-   res.write("Hello My love NodeJs lalala");
+   res.write("Cristiano Ronaldo");
    res.end();
 }).listen(8080);
