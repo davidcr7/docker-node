14f1b1150e5b0f262f4fceff1c2729d61e2c3c18
davidcr <1070631407@qq.com>
2019-02-01 14:04:40 +0800

    测试 添加shell

---
+++ docker-rb.sh
@@ -0,0 +1,49 @@
+#/bin/bash
+echo "-----------START------------------"
+
+currentDockerFile="./Dockerfile"
+
+if [ ! -d $currentDockerFile ];then
+	echo "当前目录下未找到Dockerfile文件";
+	exit;
+fi
+
+#应用名称
+app_name=$1
+
+conf_file="/opt/www/docker-sh/app.conf"
+
+function readINI()
+{
+ FILENAME=$1; SECTION=$2; KEY=$3
+ RESULT=`awk -F '=' '/\['$SECTION'\]/{a=1}a==1&&$1~/'$KEY'/{print $2;exit}' $FILENAME`
+ echo $RESULT
+}
+
+if [ ! $app_name ];then
+	echo "请输入参数";
+	exit;
+fi
+
+app_path=$(readINI $conf_file $app_name "app_path")
+app_port=$(readINI $conf_file $app_name "app_port")
+
+if [ ! $app_path -o ! $app_port ];then
+	echo "镜像不存在";
+	exit;
+fi
+
+if [ ! -d $app_path ];then
+	echo "文件夹不存在";
+	exit;
+fi	
+
+cd $app_path
+
+docker build -t $app_name -f Dockerfile .
+docker kill $app_name
+docker rm $app_name
+docker run -itd --name=$app_name -p $app_port:$app_port $app_name
+
+
+echo "-----------应用部署成功------------------"
--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(req,res){
    res.writeHead(200,{"Content-type":"text/blain"});
-   res.write("Cristiano Ronaldo DavidCR 7");
+   res.write("Cristiano Ronaldo DavidCR lalala");
    res.end();
 }).listen(8080);
