ae788823b506bd7087c2f268252521a774861154
arenacloud1101 <arenacloud1101@arenacloud.com>
2019-02-14 19:18:25 +0800

    ass

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(req,res){
    res.writeHead(200,{"Content-type":"text/blain"});
-   res.write("Cristiano Ronaldo DavidCR Come On !!!");
+   res.write("Cristiano Ronaldo DavidCR Come On Wa Wa Wa !!!");
    res.end();
 }).listen(8080);
